d380ca800a0b6c20ec8635273d9558ec978b5805
adinwirog <71881001+adinwirog@users.noreply.github.com>
2021-01-02 10:45:58 +0700

    Program fixed

--- Rengginang Mbah Mutim.py
+++ Rengginang Mbah Mutim.py
@@ -50,12 +50,12 @@ class dataKasir(Database):
         return self.res[0]
 
     def show(self):
-        self.cursor.execute("SELECT * FROM `operator kasir`")
+        self.cursor.execute("SELECT idoperator,namaoperator,nik FROM `operator kasir`")
         self.res = self.cursor.fetchall()
-        print("id \t nama operator \t\t\t NIK \t\t\t username \t\t password")
+        print("id \t nama operator \t\t\t NIK")
         print("================================================================")
         for x in self.res:
-            print(x[0],"\t",x[1],"\t\t",x[2],"\t",x[3],"\t\t",x[4])
+            print(x[0],"\t",x[1],"\t\t\t",x[2])
         print("================================================================")
 
     def insert(self,a,b,c,d):
@@ -69,10 +69,38 @@ class dataKasir(Database):
         print("Data Berhasil Ditambahkan")
 
     def update(self):
-        pass
+        self.show()
+        pilihid = str(input("\nPilih Id Operator => "))
+        self.cursor.execute("SELECT idoperator,namaoperator,nik FROM `operator kasir` WHERE idoperator = '{}'".format(pilihid))
+        self.res = self.cursor.fetchone()
+        print("\nId Operator \t\t= '{}' \nNama Operator \t\t= '{}' \nNIK \t\t= '{}'".format(self.res[0],self.res[1],self.res[2]))
+        nama = str(input("\nMasukkan Nama Operator Baru => "))
+        nik = str(input("\nMasukkan NIK Baru => "))
+        self.query = "UPDATE `operator kasir` SET namaoperator = '{}', nik = '{}' WHERE idoperator = '{}'".format(nama,nik,pilihid)
+        self.cursor.execute(self.query)
+        self.con.commit()
+        print("\nData Berhasil Diubah")
+
+    def updatepass(self):
+        self.show()
+        pilihid = str(input("\nPilih Id Operator => "))
+        self.cursor.execute("SELECT idoperator,namaoperator,nik FROM `operator kasir` WHERE idoperator = '{}'".format(pilihid))
+        self.res = self.cursor.fetchone()
+        print("\nId Operator \t\t= '{}' \nNama Operator \t\t= '{}' \nNIK \t\t= '{}'".format(self.res[0],self.res[1],self.res[2]))
+        username = str(input("\nMasukkan Username Baru => "))
+        password = str(getpass.getpass("\nMasukkan Password Baru => "))
+        self.query = "UPDATE `operator kasir` SET usernamekasir = '{}', passwordkasir = '{}' WHERE idoperator = '{}'".format(username,password,pilihid)
+        self.cursor.execute(self.query)
+        self.con.commit()
+        print("\nData Berhasil Diubah")
 
     def delete(self):
-        pass
+        self.show()
+        pilihid = str(input("\nPilih Id Operator => "))
+        self.query = "DELETE FROM `operator kasir` WHERE idoperator = '{}'".format(pilihid)
+        self.cursor.execute(self.query)
+        self.con.commit()
+        print("\nData Berhasil Dihapus")
 
 class dataAdmin(Database):
     def validasipass(self,a,c):
@@ -91,7 +119,7 @@ class dataAdmin(Database):
         return self.res
 
     def show(self):
-        self.cursor.execute("SELECT * FROM `admin`")
+        self.cursor.execute("SELECT idadmin,namaadmin FROM `admin`")
         self.res = self.cursor.fetchone()
         print(self.res)
 
@@ -104,8 +132,13 @@ class dataAdmin(Database):
         self.con.commit()
         print("Admin Telah Terdaftar")
 
-    def update(self):
-        pass
+    def updateakun(self):
+        self.show()
+        pilihid = str(input("\nPilih Id Admin => "))
+        username = str(input("\nMasukkan Username Yang Baru => "))
+        password = str(getpass.getpass("\nMasukkan Password Yang Baru => "))
+        self.query = "UPDATE `admin` SET usernameadmin = '{}', password = '{}' WHERE idadmin = '{}'".format(username,password,pilihid)
+        print("Data Berhasil Diubah")
 
 class dataProduk(Database):
     def getHarga(self,a):
@@ -130,16 +163,60 @@ class dataProduk(Database):
         for x in self.res:
             # print(x)
             print(x[0],"\t\t",x[1],"\t\t\t",x[2])
-        print("================================================================")
+        print("================================================================\n")
 
-    def insert(self):
-        pass
+    def insert(self,a,b):
+        self.a = a
+        self.b = b
+        self.query = "INSERT INTO `produk`(namabarang, hargabarang) VALUES('{}', '{}')".format(self.a,self.b)
+        self.cursor.execute(self.query)
+        self.con.commit()
+        print("Data Berhasil Ditambahkan")
 
-    def update(self):
-        pass
+    def updateHarga(self):
+        self.show()
+        pilihid = str(input("\nPilih Id Produk => "))
+        self.cursor.execute("SELECT idbarang,namabarang,hargabarang FROM `produk` WHERE idbarang = '{}'".format(pilihid))
+        self.res = self.cursor.fetchone()
+        print("\nId Produk \t\t= '{}' \nNama Produk \t\t= '{}' \nHarga Produk \t\t= '{}'".format(self.res[0],self.res[1],self.res[2]))
+        harga = str(input("\nMasukkan Harga Baru => "))
+        self.query = "UPDATE `produk` SET hargabarang = '{}' WHERE idbarang = '{}'".format(harga,pilihid)
+        self.cursor.execute(self.query)
+        self.con.commit()
+        print("\nHarga Berhasil Diubah")
+
+    def updateNama(self):
+        self.show()
+        pilihid = str(input("\nPilih Id Produk => "))
+        self.cursor.execute("SELECT idbarang,namabarang,hargabarang FROM `produk` WHERE idbarang = '{}'".format(pilihid))
+        self.res = self.cursor.fetchone()
+        print("\nId Produk \t\t= '{}' \nNama Produk \t\t= '{}' \nHarga Produk \t\t= '{}'".format(self.res[0],self.res[1],self.res[2]))
+        nama = str(input("\nMasukkan Nama Produk Baru => "))
+        self.query = "UPDATE `produk` SET namabarang = '{}' WHERE idbarang = '{}'".format(nama,pilihid)
+        self.cursor.execute(self.query)
+        self.con.commit()
+        print("\nNama Produk Berhasil Diubah")
+
+    def updateall(self):
+        self.show()
+        pilihid = str(input("\nPilih Id Produk => "))
+        self.cursor.execute("SELECT idbarang,namabarang,hargabarang FROM `produk` WHERE idbarang = '{}'".format(pilihid))
+        self.res = self.cursor.fetchone()
+        print("\nId Produk \t\t= '{}' \nNama Produk \t\t= '{}' \nHarga Produk \t\t= '{}'".format(self.res[0],self.res[1],self.res[2]))
+        nama = str(input("\nMasukkan Nama Produk Baru => "))
+        harga = str(input("\nMasukkan Harga Baru => "))
+        self.query = "UPDATE `produk` SET namabarang = '{}', hargabarang = '{}' WHERE idbarang = '{}'".format(nama,harga,pilihid)
+        self.cursor.execute(self.query)
+        self.con.commit()
+        print("\nData Produk Berhasil Diubah")
 
     def delete(self):
-        pass
+        self.show()
+        pilihid = str(input("\nPilih Id Produk => "))
+        self.query = "DELETE FROM `produk` WHERE idbarang = '{}'".format(pilihid)
+        self.cursor.execute(self.query)
+        self.con.commit()
+        print("\nData Berhasil Dihapus")
 
 class dataPembeli(Database):
     def cekPembelipass(self,a,b,c):
@@ -172,8 +249,11 @@ class dataPembeli(Database):
     def show(self):
         self.cursor.execute("SELECT * FROM `pembeli`")
         self.res = self.cursor.fetchall()
+        print("id Pembeli \t Nama Pembeli \t\t\t Telepon \t\t\t Alamat")
+        print("==========================================================================================")
         for x in self.res:
-            print(x)
+            print(x[0],"\t\t",x[1],"\t\t\t",x[2],"\t\t\t",x[3])
+        print("==========================================================================================\n")
 
     def insert(self,a,b,c):
         self.a = a
@@ -185,17 +265,36 @@ class dataPembeli(Database):
         print("Berhasil memasukkan data")
 
     def update(self):
-        pass
+        self.show()
+        pilihid = str(input("\nPilih Id Pembeli => "))
+        self.cursor.execute("SELECT idpembeli,namapembeli,telepon,alamat FROM `pembeli` WHERE idpembeli = '{}'".format(pilihid))
+        self.res = self.cursor.fetchone()
+        print("\nId Pembeli \t\t= '{}' \nNama Pembeli \t\t= '{}' \nTelepon \t\t= '{}' \nAlamat \t\t= '{}'".format(self.res[0],self.res[1],self.res[2],self.res[3]))
+        nama = str(input("\nMasukkan Nama Pembeli => "))
+        telepon = str(input("\nMasukkan Nomor Telepon => "))
+        alamat = str(input("\nMasukkan Alamat => "))
+        self.query = "UPDATE `pembeli` SET namapembeli = '{}', telepon = '{}', alamat = '{}' WHERE idpembeli = '{}'".format(nama,telepon,alamat,pilihid)
+        self.cursor.execute(self.query)
+        self.con.commit()
+        print("\nData Pembeli Berhasil Diubah")
 
     def delete(self):
-        pass 
+        self.show()
+        pilihid = str(input("\nPilih Id Pembeli => "))
+        self.query = "DELETE FROM `pembeli` WHERE idpembeli = '{}'".format(pilihid)
+        self.cursor.execute(self.query)
+        self.con.commit()
+        print("\nData Berhasil Dihapus")
 
 class dataTransaksi(Database):
     def show(self):
-        self.cursor.execute("SELECT * FROM `transaksi`")
+        self.cursor.execute("SELECT namaoperator, namabarang, namapembeli, kuantitas, totalharga, tanggaltransaksi FROM transaksi JOIN pembeli ON pembeli.idpembeli = transaksi.idpembeli JOIN `operator kasir` ON `operator kasir`.`idoperator` = transaksi.idoperator JOIN produk ON produk.idbarang = transaksi.idbarang ORDER BY tanggaltransaksi DESC")
         self.res = self.cursor.fetchall()
+        print("Nama Operator \t\t Nama Barang \t Nama Pembeli \t\t Kuantitas \t Total Harga \t Tanggal")
+        print("=======================================================================================================================")
         for x in self.res:
-            print(x)
+            print(x[0],"\t",x[1],"\t",x[2],"\t\t\t",x[3],"\t\t",x[4],"\t\t",x[5])
+        print("=======================================================================================================================\n")
 
     def insert(self,a,b,c,d,e):
         self.a = a
@@ -208,11 +307,12 @@ class dataTransaksi(Database):
         self.con.commit()
 
     def delete(self):
-        pass
+        print("\nData Mulai Untuk Dihapus")
+        self.cursor.execute("TRUNCATE TABLE `transaksi`")
+        self.con.commit()
+        print("\nData Berhasil Dihapus Semua")
 
-class dataRiwayat(Database):
-    def show(self):
-        pass
+
 
 class Sistem:
     namakasir = ""
@@ -362,8 +462,171 @@ class Sistem:
     def menuAdmin(self):
         print("\n====== Menu Utama Admin ======")
         print("\nPilih Menu Di Sini\n")
-        print("1. Daftar Operator \n2. Daftar Produk \n3. Daftar Pembeli")
+        print("1. Menu Operator \n2. Daftar Produk \n3. Daftar Pembeli")
         print("4. Daftar Transaksi \n5. Recovery Admin \n6. Keluar")
+        masuk = str(input("Input Di Sini => "))
+        if masuk == str(1):
+            self.adminKasir()
+        elif masuk == str(2):
+            self.adminProduk()
+        elif masuk == str(3):
+            self.adminPembeli()
+        elif masuk == str(4):
+            self.adminTransaksi()
+        elif masuk == str(5):
+            self.recoverAdmin()
+        elif masuk == str(6):
+            print("\n====== Terima Kasih Telah Menggunakan ======")
+        else:
+            print("\nInput salah, Coba lagi")
+            self.menuAdmin()
+
+    def adminKasir(self):
+        k = dataKasir()
+        print("\n====== Menu Manipulasi Operator ======")
+        print("\nPilih Menu Di Sini\n")
+        print("1. Daftar Operator \n2. Tambah Data Operator \n3. Ubah Data Operator \n4. Hapus Data Operator")
+        print("5. Main Menu \n6. Keluar")
+        masuk = str(input("\nInput Di Sini => "))
+        if masuk == str(1):
+            k.show()
+            ok = str(input("\nPress Any Input "))
+            self.adminKasir()
+        elif masuk == str(2):
+            a = str(input("\nMasukkan Nama Kasir => "))
+            b = str(input("\nMasukkan NIK => "))
+            c = str(input("\nMasukkan Username Sementara => "))
+            d = str(getpass.getpass("\nMasukkan Password Sementara => "))
+            k.insert(a,b,c,d)
+            k.show()
+            ok = str(input("\nPress Any Input "))
+            self.adminKasir()
+        elif masuk == str(3):
+            print("\nPilih Ubah Data")
+            print("\n1. Ubah Biodata \n2. Reset Username Dan Password \n3. Return")
+            pilihan = str(input("\nInput Di Sini => "))
+            if pilihan == str(1):
+                k.update()
+                ok = str(input("\nPress Any Input "))
+                self.adminKasir()
+            elif pilihan == str(2):
+                k.updatepass()
+                ok = str(input("\nPress Any Input "))
+                self.adminKasir()
+            else:
+                self.adminKasir()
+        elif masuk == str(4):
+            k.delete()
+            ok = str(input("\nPress Any Input "))
+            self.adminKasir()
+        elif masuk == str(5):
+            self.menuAdmin()
+        elif masuk == str(6):
+            print("\n====== Terima Kasih Telah Menggunakan ======")
+        else:
+            print("\nInput salah, Coba lagi")
+            self.adminKasir()
+
+    def adminProduk(self):
+        p = dataProduk()
+        print("\n====== Menu Manipulasi Produk ======")
+        print("\nPilih Menu Di Sini\n")
+        print("1. Daftar Produk \n2. Tambah Produk \n3. Ubah Data Produk \n4. Hapus Data Produk")
+        print("5. Main Menu \n6. Keluar")
+        masuk = str(input("Input Di Sini => "))
+        if masuk == str(1):
+            p.show()
+            ok = str(input("\nPress Any Input "))
+            self.adminProduk()
+        elif masuk == str(2):
+            a = str(input("Masukkan Nama Produk => "))
+            b = str(input("Masukkan Harga Per Buah => "))
+            p.insert(a,b)
+            p.show()
+            ok = str(input("\nPress Any Input "))
+            self.adminProduk()
+        elif masuk == str(3):
+            p.updateall()
+            ok = str(input("\nPress Any Input "))
+            self.adminProduk()
+        elif masuk == str(4):
+            p.delete()
+            self.adminProduk()
+        elif masuk == str(5):
+            self.menuAdmin()
+        elif masuk == str(6):
+            print("\n====== Terima Kasih Telah Menggunakan ======")
+        else:
+            print("\nInput salah, Coba lagi")
+            self.adminProduk()
+
+    def adminPembeli(self):
+        c = dataPembeli()
+        print("\n====== Menu Manipulasi Pembeli ======")
+        print("\nPilih Menu Di Sini\n")
+        print("1. Daftar Pembeli \n2. Hapus Data Pembeli \n3. Ubah Data Pembeli")
+        print("4. Main Menu \n5. Keluar")
+        masuk = str(input("Input Di Sini => "))
+        if masuk == str(1):
+            c.show()
+            ok = str(input("\nPress Any Input "))
+            self.adminPembeli()
+        elif masuk == str(2):
+            c.delete()
+            ok = str(input("\nPress Any Input "))
+            self.adminPembeli()
+        elif masuk == str(3):
+            c.update()
+            ok = str(input("\nPress Any Input "))
+            self.adminPembeli()
+        elif masuk == str(4):
+            self.menuAdmin()
+        elif masuk == str(5):
+            print("\n====== Terima Kasih Telah Menggunakan ======")
+        else:
+            print("\nInput salah, Coba lagi")
+            self.adminPembeli()
+
+    def adminTransaksi(self):
+        t = dataTransaksi()
+        print("\n====== Menu Manipulasi Pembeli ======")
+        print("\nPilih Menu Di Sini\n")
+        print("1. Lihat Riwayat Transaksi \n2. Hapus Riwayat Transaksi")
+        print("3. Main Menu \n4. Keluar")
+        masuk = str(input("Input Di Sini => "))
+        if masuk == str(1):
+            t.show()
+            ok = str(input("\nPress Any Input "))
+            self.adminTransaksi()
+        elif masuk == str(2):
+            print("\nData Akan Dihapus Semua. Anda Yakin?")
+            print("\n1. Ya \n2. Tidak")
+            pilih = str(input("\nInput Di Sini => "))
+            if pilih == str(1):
+                t.delete()
+                ok = str(input("\nPress Any Input "))
+                self.adminTransaksi
+            else:
+                self.adminTransaksi
+        elif masuk == str(3):
+            self.menuAdmin()
+        elif masuk == str(4):
+            print("\n====== Terima Kasih Telah Menggunakan ======")
+        else:
+            print("\nInput salah, Coba lagi")
+            self.adminTransaksi()
+
+    def recoverAdmin(self):
+        m = dataAdmin()
+        print("\nReset Akun Admin?")
+        print("1. Ya \n2. Tidak")
+        masuk = str(input("\n Input Di Sini => "))
+        if masuk == str(1):
+            m.updateakun()
+            ok = str(input("\nPress Any Input "))
+            self.menuAdmin()
+        else:
+            self.menuAdmin
 
     def menuKasir(self,a):
         self.a = a
@@ -437,6 +700,7 @@ class transaksiKasir:
         listKuantitas = []
         listHarga = []
         while flag == False:
+            get.show()
             produk = str(input("Masukkan ID Produk Yang Ingin Dibeli => "))
             listProduk.append(produk)
             kuantitas = int(input("Jumlah yang ingin dipesan => "))
@@ -535,12 +799,15 @@ class transaksiKasir:
         bayar = int(input("\nBerapa Uang Yang dibayar? => "))
 
         kembalian = bayar - totalHarga
+        now = datetime.now()
+        formatted_date = now.strftime('%d-%m-%Y %H:%M:%S')
 
         if kembalian >= 0 :
             print("\n================ STRUK PESANAN ====================")
+            print("Tanggal Dan Waktu : ", formatted_date) 
             print("Atas Nama : ", self.nama)
             print("Alamat : ", self.alamat)
-            print("Dengan Operator Kasir :", self.namakasir) 
+            print("Dengan Operator Kasir :", self.namakasir)
             print("Nama Produk \t|| Jumlah Pesanan || Jumlah Harga Per Pesan")
             print("--------------------------------------------------")
             for i in range(len(namaProduk)):
@@ -557,10 +824,6 @@ class transaksiKasir:
 
 
         
-        
-
-
-
 
 p1 = Sistem()
 p1.validasiAdmin()
